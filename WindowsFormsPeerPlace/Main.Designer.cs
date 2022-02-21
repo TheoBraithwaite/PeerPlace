5b859dcbacaac9b672cb215a2ef646734c29c12e
Theo at home <theobraith@gmail.com>
2022-02-21 17:08:04 +1300

    Main window setup using DataGridView.

--- WindowsFormsPeerPlace/Main.Designer.cs
+++ WindowsFormsPeerPlace/Main.Designer.cs
@@ -28,7 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.employeeGrid = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuFile = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.employeeGrid)).BeginInit();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // employeeGrid
+            // 
+            this.employeeGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.employeeGrid.Location = new System.Drawing.Point(12, 89);
+            this.employeeGrid.Name = "employeeGrid";
+            this.employeeGrid.RowTemplate.Height = 25;
+            this.employeeGrid.Size = new System.Drawing.Size(905, 447);
+            this.employeeGrid.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(318, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(255, 56);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "PeerPlace";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuFile});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(929, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuFile
+            // 
+            this.menuFile.Name = "menuFile";
+            this.menuFile.Size = new System.Drawing.Size(37, 20);
+            this.menuFile.Text = "File";
             // 
             // Main
             // 
@@ -36,12 +77,26 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Lavender;
             this.ClientSize = new System.Drawing.Size(929, 571);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.employeeGrid);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Main";
             this.Text = "PeerPlace - Management Console";
+            this.Load += new System.EventHandler(this.Main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.employeeGrid)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private DataGridView employeeGrid;
+        private Label label1;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem menuFile;
     }
 }
--- WindowsFormsPeerPlace/Main.cs
+++ WindowsFormsPeerPlace/Main.cs
@@ -14,9 +14,103 @@ namespace WindowsFormsPeerPlace
 {
     public partial class Main : Form
     {
+
+        List<int> idList = new List<int>();
+        List<string> firstNameList = new List<string>();
+        List<string> lastNameList = new List<string>();
+        List<string> emailList = new List<string>();
+        List<int> numberList = new List<int>();
+        List<string> genderList = new List<string>();
+        List<string> addressList = new List<string>();
+
         public Main()
         {
             InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+
+            string line = "";
+            string[] csvArray;
+
+            //employeeGrid.Columns.Add("Index", "Index");
+            //employeeGrid.Columns.Add("Value", "Dice Value");
+            //int[] theData = new int[] { 5, 2, 1, 5, 4, 1, 3, 1 };
+
+            //for (int i = 0; i < theData.Length; i++)
+            //{
+            //employeeGrid.Rows.Add(new object[] { i + 1, theData[i] });
+            //}
+
+            string openLocation = @"C:\Users\BEASTY-BOY\Desktop\employeeList.csv";
+            using var reader = new StreamReader(openLocation);
+            {
+                int id;
+                string firstName;
+                string lastName;
+                string email;
+                int employeeNum;
+                string gender;
+                string streetAddress;
+
+                while (!reader.EndOfStream)
+                {
+                    try
+                    {
+                        //Read a line from the file
+                        line = reader.ReadLine();
+                        //Split the clients in the line using an array
+                        csvArray = line.Split(',');
+                        //Check if the array has the correct number of elements
+                        if (csvArray.Length == 7)
+                        {
+
+                            //Extract the data into separate variables
+                            id = int.Parse(csvArray[0]);
+                            firstName = csvArray[1];
+                            lastName = csvArray[2];
+                            email = csvArray[3];
+                            employeeNum = int.Parse(csvArray[4]);
+                            gender = csvArray[5];
+                            streetAddress = csvArray[6];
+
+                            //Add clients to list.
+                            idList.Add(id);
+
+                            //Add password to list.
+                            firstNameList.Add(firstName);
+                            lastNameList.Add(lastName);
+                            emailList.Add(email);
+                            numberList.Add(employeeNum);
+                            genderList.Add(gender);
+                        }
+                        else
+                        {
+                            //LabelMethod(errorFile);
+                            MessageBox.Show("Error in file.");
+                        }
+                    }
+                    catch
+                    {
+                        //LabelMethod(errorCrash);
+                        MessageBox.Show("Fatal error.");
+                    }
+                }
+                MessageBox.Show("Completed.");
+
+                employeeGrid.Columns.Add("Index", "ID");
+                employeeGrid.Columns.Add("Value", "First Name");
+                employeeGrid.Columns.Add("Value", "Last Name");
+                employeeGrid.Columns.Add("Value", "Email");
+                employeeGrid.Columns.Add("Value", "Employee ID");
+                employeeGrid.Columns.Add("Value", "Gender");
+
+                for (int i = 0; i < idList.Count; i++)
+                {
+                    employeeGrid.Rows.Add(new object[] { i + 1, firstNameList[i], lastNameList[i], emailList[i], numberList[i], genderList[i]});
+                }
+            }
         }
     }
 }
